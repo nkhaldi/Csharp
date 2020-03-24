3fb5a77b0b16da058bca8f62785905cdc941e9da
Narek <mnk80898@gmail.com>
2020-03-24 23:01:34 +0300

    default commit messege

--- hello.cs
+++ hello.cs
@@ -1,7 +1,9 @@
 using System;
 
-class MailClass {
-	static void Main(string[] args) {
+class MailClass
+{
+	static void Main(string[] args)
+	{
 		Console.WriteLine("What is your name?");
 		string name = Console.ReadLine();
 		Console.WriteLine($"Hello, {name}!");
